1becc313a126e7ca7acbce12b2efe27c94a4c2dd
Ankush Singh <ankushsahil45@gmail.com>
2022-08-08 12:34:34 +0530

    updated function

--- script/model.js
+++ script/model.js
@@ -1,29 +1,24 @@
-var salaryOperations = {
-  hra(BasicSalary) {
-    return BasicSalary * 0.5;
+const salaryOperations = {
+  BasicSalary: 0,
+  HRA() {
+    return this.BasicSalary * 0.5;
   },
-  da(BasicSalary) {
-    return BasicSalary * 0.4;
+  DA() {
+    return this.BasicSalary * 0.4;
   },
-  ta(BasicSalary) {
-    return BasicSalary * 0.3;
+  TA() {
+    return this.BasicSalary * 0.3;
   },
-  ma(BasicSalary) {
-    return BasicSalary * 0.6;
+  MA() {
+    return this.BasicSalary * 0.6;
   },
-  gs(BasicSalary) {
-    return (
-      BasicSalary +
-      salaryOperations.hra(BasicSalary) +
-      salaryOperations.da(BasicSalary) +
-      salaryOperations.ta(BasicSalary) +
-      salaryOperations.ma(BasicSalary)
-    );
+  GS() {
+    return BasicSalary + this.HRA() + this.DA() + this.TA() + this.MA();
   },
-  tax(BasicSalary) {
+  TAX() {
     return (BasicSalary * 10) / 100;
   },
-  ns(BasicSalary) {
-    return salaryOperations.gs(BasicSalary) - salaryOperations.tax(BasicSalary);
+  NS() {
+    return this.GS() - this.TAX();
   },
 };
